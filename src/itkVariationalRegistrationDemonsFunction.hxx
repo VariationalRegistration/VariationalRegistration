4d1e26f0e2e5ff4557e8eafb60ccfdd2e4533c02
ehrhardt <ehrhardt@imi.uni-luebeck.de>
2014-04-14 12:32:09 +0200

    removed bug in regarding the DenominatorThreshold in DemonsFunction.

--- src/itkVariationalRegistrationDemonsFunction.hxx
+++ src/itkVariationalRegistrationDemonsFunction.hxx
@@ -172,14 +172,12 @@ VariationalRegistrationDemonsFunction< TFixedImage, TMovingImage, TDisplacementF
     const double denominator = sqr_speedValue / m_Normalizer + gradientSquaredMagnitude;
 
     if( denominator < m_DenominatorThreshold )
-      {
       update = m_ZeroUpdateReturn;
-      }
-
-    for( unsigned int j = 0; j < ImageDimension; j++ )
-      {
-      update[j] = speedValue * gradient[j] / denominator;
-      }
+    else
+      for( unsigned int j = 0; j < ImageDimension; j++ )
+        {
+        update[j] = speedValue * gradient[j] / denominator;
+        }
     }
 
   // Update the global data (metric etc.)
